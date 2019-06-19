8efa28316f2c2fcb9764a3104accd274607a33fe
whdlgp <whdlgp@gmail.com>
2019-06-20 02:29:23 +0900

    Add more test case

--- spherical_bundle_adjuster.cpp
+++ spherical_bundle_adjuster.cpp
@@ -266,6 +266,305 @@ void spherical_bundle_adjuster::do_bundle_adjustment(const cv::Mat &im_left, con
     write_d_circle(im_left, init_d4, left_key4, "d_found4");
     write_log_d(init_d4, "log_d4");
 
+    // 5th initial value
+    vector<KeyPoint> left_key5;
+    vector<KeyPoint> right_key5;
+    vector<Point3d> key_point_left_rect5;
+    vector<Point3d> key_point_right_rect5;
+    vector<array<double, 2>> init_d5;
+    int match_size5 = 0;
+    for(int i = 0; i < match_size; i++)
+    {
+        if(init_d[i][0] >= 0)
+        {
+            match_size5++;
+            left_key5.push_back(left_key[i]);
+            right_key5.push_back(right_key[i]);
+            key_point_left_rect5.push_back(key_point_left_rect[i]);
+            key_point_right_rect5.push_back(key_point_right_rect[i]);
+            init_d5.push_back(init_d[i]);
+        }
+    }
+    double init_rot5[3] = {init_rot[0], init_rot[1], init_rot[2]};
+    double init_tran5[3] = {init_tran[0], init_tran[1], init_tran[2]};
+
+    for(int i = 0; i < 2; i++)
+    {
+        for(int i = 0; i < match_size5; i++)
+        {
+            if(init_d5[i][0] < 0)
+            {
+                match_size5--;
+                left_key5.erase(left_key5.begin()+i);
+                right_key5.erase(right_key5.begin()+i);
+                key_point_left_rect5.erase(key_point_left_rect5.begin()+i);
+                key_point_right_rect5.erase(key_point_right_rect5.begin()+i);
+                init_d5.erase(init_d5.begin()+i);
+            }
+        }
+
+        solve_problem(options
+                    , key_point_left_rect5
+                    , key_point_right_rect5
+                    , init_rot5
+                    , init_tran5
+                    , init_d5
+                    , match_size5);
+    }
+
+    write_d_circle(im_left, init_d5, left_key5, "d_found5");
+    write_log_d(init_d5, "log_d5");
+
+    // 6th initial value
+    vector<KeyPoint> left_key6;
+    vector<KeyPoint> right_key6;
+    vector<Point3d> key_point_left_rect6;
+    vector<Point3d> key_point_right_rect6;
+    vector<array<double, 2>> init_d6;
+    int match_size6 = 0;
+    for(int i = 0; i < match_size; i++)
+    {
+        if(init_d[i][0] >= 0)
+        {
+            match_size6++;
+            left_key6.push_back(left_key[i]);
+            right_key6.push_back(right_key[i]);
+            key_point_left_rect6.push_back(key_point_left_rect[i]);
+            key_point_right_rect6.push_back(key_point_right_rect[i]);
+            init_d6.push_back(init_d[i]);
+        }
+    }
+    double init_rot6[3] = {init_rot[0], init_rot[1], init_rot[2]};
+    double init_tran6[3] = {init_tran[0], init_tran[1], init_tran[2]};
+
+    for(int i = 0; i < 2; i++)
+    {
+        for(int i = 0; i < match_size6; i++)
+        {
+            if(init_d6[i][0] < 0)
+            {
+                match_size6--;
+                left_key6.erase(left_key6.begin()+i);
+                right_key6.erase(right_key6.begin()+i);
+                key_point_left_rect6.erase(key_point_left_rect6.begin()+i);
+                key_point_right_rect6.erase(key_point_right_rect6.begin()+i);
+                init_d6.erase(init_d6.begin()+i);
+            }
+        }
+
+        double max_d = 0, min_d = 0;
+        int max_idx = 0, min_idx = 0;
+        for(int i = 0; i < match_size6; i++)
+        {
+            if(init_d6[i][0] > max_d) 
+            {
+                max_d = init_d6[i][0];
+                max_idx = i;
+            }
+            if(init_d6[i][0] < min_d)
+            {
+                min_d = init_d6[i][0];
+                min_idx = i;
+            }
+        }
+        match_size6--;
+        left_key6.erase(left_key6.begin()+max_idx);
+        right_key6.erase(right_key6.begin()+max_idx);
+        key_point_left_rect6.erase(key_point_left_rect6.begin()+max_idx);
+        key_point_right_rect6.erase(key_point_right_rect6.begin()+max_idx);
+        init_d6.erase(init_d6.begin()+max_idx);
+
+        match_size6--;
+        left_key6.erase(left_key6.begin()+min_idx);
+        right_key6.erase(right_key6.begin()+min_idx);
+        key_point_left_rect6.erase(key_point_left_rect6.begin()+min_idx);
+        key_point_right_rect6.erase(key_point_right_rect6.begin()+min_idx);
+        init_d6.erase(init_d6.begin()+min_idx);
+        
+        solve_problem(options
+                    , key_point_left_rect6
+                    , key_point_right_rect6
+                    , init_rot6
+                    , init_tran6
+                    , init_d6
+                    , match_size6);
+    }
+
+    write_d_circle(im_left, init_d6, left_key6, "d_found6");
+    write_log_d(init_d6, "log_d6");
+
+
+    // 7th initial value
+    vector<KeyPoint> left_key7;
+    vector<KeyPoint> right_key7;
+    vector<Point3d> key_point_left_rect7;
+    vector<Point3d> key_point_right_rect7;
+    vector<array<double, 2>> init_d7;
+    int match_size7 = 0;
+    for(int i = 0; i < match_size; i++)
+    {
+        if(init_d[i][0] >= 0)
+        {
+            match_size7++;
+            left_key7.push_back(left_key[i]);
+            right_key7.push_back(right_key[i]);
+            key_point_left_rect7.push_back(key_point_left_rect[i]);
+            key_point_right_rect7.push_back(key_point_right_rect[i]);
+            array<double, 2> tmp = {1, 1};
+            init_d7.push_back(tmp);
+        }
+    }
+    double init_rot7[3] = {expected_roll/180*M_PI, expected_pitch/180*M_PI, expected_yaw/180*M_PI};
+    double init_tran7[3] = {0.0, 0.0, 0.0};
+
+    for(int i = 0; i < 2; i++)
+    {
+        for(int i = 0; i < match_size7; i++)
+        {
+            if(init_d7[i][0] < 0)
+            {
+                match_size7--;
+                left_key7.erase(left_key7.begin()+i);
+                right_key7.erase(right_key7.begin()+i);
+                key_point_left_rect7.erase(key_point_left_rect7.begin()+i);
+                key_point_right_rect7.erase(key_point_right_rect7.begin()+i);
+                init_d7.erase(init_d7.begin()+i);
+            }
+        }
+
+        double max_d = 0, min_d = 0;
+        int max_idx = 0, min_idx = 0;
+        for(int i = 0; i < match_size7; i++)
+        {
+            if(init_d7[i][0] > max_d) 
+            {
+                max_d = init_d7[i][0];
+                max_idx = i;
+            }
+            if(init_d7[i][0] < min_d)
+            {
+                min_d = init_d7[i][0];
+                min_idx = i;
+            }
+        }
+        match_size7--;
+        left_key7.erase(left_key7.begin()+max_idx);
+        right_key7.erase(right_key7.begin()+max_idx);
+        key_point_left_rect7.erase(key_point_left_rect7.begin()+max_idx);
+        key_point_right_rect7.erase(key_point_right_rect7.begin()+max_idx);
+        init_d7.erase(init_d7.begin()+max_idx);
+
+        match_size7--;
+        left_key7.erase(left_key7.begin()+min_idx);
+        right_key7.erase(right_key7.begin()+min_idx);
+        key_point_left_rect7.erase(key_point_left_rect7.begin()+min_idx);
+        key_point_right_rect7.erase(key_point_right_rect7.begin()+min_idx);
+        init_d7.erase(init_d7.begin()+min_idx);
+        
+        solve_problem(options
+                    , key_point_left_rect7
+                    , key_point_right_rect7
+                    , init_rot7
+                    , init_tran7
+                    , init_d7
+                    , match_size7);
+    }
+
+    write_d_circle(im_left, init_d7, left_key7, "d_found7");
+    write_log_d(init_d7, "log_d7");
+
+    // 8th initial value
+    vector<KeyPoint> left_key8;
+    vector<KeyPoint> right_key8;
+    vector<Point3d> key_point_left_rect8;
+    vector<Point3d> key_point_right_rect8;
+    vector<array<double, 2>> init_d8;
+    int match_size8 = 0;
+    for(int i = 0; i < match_size; i++)
+    {
+        if(init_d[i][0] >= 0)
+        {
+            match_size8++;
+            left_key8.push_back(left_key[i]);
+            right_key8.push_back(right_key[i]);
+            key_point_left_rect8.push_back(key_point_left_rect[i]);
+            key_point_right_rect8.push_back(key_point_right_rect[i]);
+            array<double, 2> tmp = {1, 1};
+            init_d8.push_back(tmp);
+        }
+    }
+    double init_rot8[3] = {expected_roll/180*M_PI, expected_pitch/180*M_PI, expected_yaw/180*M_PI};
+    double init_tran8[3] = {0.0, 0.0, 0.0};
+
+    for(int i = 0; i < 2; i++)
+    {
+        for(int i = 0; i < match_size8; i++)
+        {
+            if(init_d8[i][0] < 0)
+            {
+                match_size8--;
+                left_key8.erase(left_key8.begin()+i);
+                right_key8.erase(right_key8.begin()+i);
+                key_point_left_rect8.erase(key_point_left_rect8.begin()+i);
+                key_point_right_rect8.erase(key_point_right_rect8.begin()+i);
+                init_d8.erase(init_d8.begin()+i);
+            }
+        }
+
+        double max_d = 0, min_d = 0;
+        int max_idx = 0, min_idx = 0;
+        for(int i = 0; i < match_size8; i++)
+        {
+            if(init_d8[i][0] > max_d) 
+            {
+                max_d = init_d8[i][0];
+                max_idx = i;
+            }
+            if(init_d8[i][0] < min_d)
+            {
+                min_d = init_d8[i][0];
+                min_idx = i;
+            }
+        }
+        match_size8--;
+        left_key8.erase(left_key8.begin()+max_idx);
+        right_key8.erase(right_key8.begin()+max_idx);
+        key_point_left_rect8.erase(key_point_left_rect8.begin()+max_idx);
+        key_point_right_rect8.erase(key_point_right_rect8.begin()+max_idx);
+        init_d8.erase(init_d8.begin()+max_idx);
+
+        match_size8--;
+        left_key8.erase(left_key8.begin()+min_idx);
+        right_key8.erase(right_key8.begin()+min_idx);
+        key_point_left_rect8.erase(key_point_left_rect8.begin()+min_idx);
+        key_point_right_rect8.erase(key_point_right_rect8.begin()+min_idx);
+        init_d8.erase(init_d8.begin()+min_idx);
+
+        init_rot8[0] = expected_roll/180*M_PI;
+        init_rot8[1] = expected_pitch/180*M_PI;
+        init_rot8[2] = expected_yaw/180*M_PI;
+        init_tran8[0] = 0.0;
+        init_tran8[1] = 0.0;
+        init_tran8[2] = 0.0;
+        init_d8.clear();
+        for(int i = 0; i < match_size8; i++)
+        {
+            array<double, 2> tmp = {1, 1};
+            init_d8.push_back(tmp);
+        }
+        
+        solve_problem(options
+                    , key_point_left_rect8
+                    , key_point_right_rect8
+                    , init_rot8
+                    , init_tran8
+                    , init_d8
+                    , match_size8);
+    }
+
+    write_d_circle(im_left, init_d8, left_key8, "d_found8");
+    write_log_d(init_d8, "log_d8");
+
     // Save test match image and log file
     string file_name;
     stringstream file_name_stream(file_name);
